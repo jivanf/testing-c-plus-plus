dcb32f94c254c969601c929127ef5ab2ee1846c9
Jivan <minecraftjivan@gmail.com>
2017-04-10 14:46:15 -0700

    hello world program with c++

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  essentialtraining
+//
+//  Created by Jivan Flores on 4/10/17.
+//  Copyright © 2017 Jivan Flores. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    puts("Hello World");
+    return 0;
+}
